2e7120db9b414aa40a27eacca150feb479198bb0
Isaac Bowen <ikebowen@gmail.com>
2020-11-03 15:00:06 -0700

    add a validity check

---
+++ test.js
@@ -0,0 +1,53 @@
+const dirCompare = require('dir-compare')
+const fs = require('fs')
+const glob = require('glob')
+
+const util = require('./util')
+
+const tasksFiles = glob.sync('tasks/*')
+
+const invalidFilenames = tasksFiles.filter(f => !f.match(/^tasks\/[0-9a-z-]+\.json$/))
+
+if (invalidFilenames.length > 0) {
+  console.error(`Invalid filenames in tasks/:\n${invalidFilenames.join("\n")}`)
+  throw `Found ${invalidFilenames.length} unexpected file(s) in tasks/`
+}
+
+tasksFiles.forEach(path => {
+  const json = fs.readFileSync(path, 'UTF-8')
+
+  let data;
+
+  try {
+    data = JSON.parse(json)
+  } catch (error) {
+    throw `Found invalid JSON in ${path}`
+  }
+
+  if (!json.match(/^  "/m)) {
+    throw `Found a JSON file that appears to not be formatted with two-space indentation: ${path}`
+  }
+
+  if (!json.match(/\n$/)) {
+    throw `Found a JSON file that does not have a trailing newline: ${path}`
+  }
+})
+
+try {
+  util.buildDocs('.test-docs', {silent: true})
+} catch (error) {
+  fs.rmdirSync('.test-docs', {recursive: true})
+
+  throw `Caught an error while building to .test-docs: ${error}`
+}
+
+const comparison = dirCompare.compareSync('docs', '.test-docs', {compareContent: true})
+fs.rmdirSync('.test-docs', {recursive: true})
+
+if (!comparison.same) {
+  console.error(`Differing content:`, comparison.diffSet.filter(d => d.state !== 'equal'))
+
+  throw './docs directory appears to not be up to date; please run `node ./build.js`'
+}
+
+console.log('Everything looks good! 🚀')
---
+++ util.js
@@ -0,0 +1,96 @@
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
+
+module.exports = {}
+
+module.exports.buildDocs = (docsDir = 'docs', options = {}) =>{
+  const taskExports = glob.sync('tasks/*.json')
+  const taskIndexMdLines = []
+
+  if (fs.existsSync(docsDir)) {
+    fs.rmdirSync(docsDir, {recursive: true})
+  }
+
+  fs.mkdirSync(docsDir)
+
+  taskExports.forEach(taskJsonPath => {
+    try {
+      const taskJson = fs.readFileSync(taskJsonPath, 'UTF-8')
+      const taskHandle = taskJsonPath.match(/\/([\w-]+)\.json$/)[1]
+      const taskDocsDir = `${docsDir}/${taskHandle}`
+      const task = JSON.parse(taskJson)
+
+      fs.mkdirSync(taskDocsDir)
+
+      const description = `${task.docs}`.split(/(\r?\n){2,}/)[0];
+
+      taskIndexMdLines.push(`* [${task.name}](./${taskHandle})`)
+
+      // write script.liquid
+      fs.writeFileSync(`${taskDocsDir}/script.liquid`, task.script)
+
+      // build README.md
+      let readmeMd = `# ${task.name}`
+
+      readmeMd += `\n\n* [task.json](../../tasks/${taskHandle}.json) (for import/export)`
+
+      if (task.script) {
+        readmeMd += `\n* [Task script](./script.liquid)`
+        fs.writeFileSync(`${taskDocsDir}/script.liquid`, task.script.trim() + `\n`)
+      }
+
+      if (task.online_store_javascript) {
+        readmeMd += `\n* [Task online store javascript](./online_store_javascript.js.liquid)`
+        fs.writeFileSync(`${taskDocsDir}/online_store_javascript.js.liquid`, task.online_store_javascript.trim() + `\n`)
+      }
+
+      if (task.order_status_javascript) {
+        readmeMd += `\n* [Task order status javascript](./order_status_javascript.js.liquid)`
+        fs.writeFileSync(`${taskDocsDir}/order_status_javascript.js.liquid`, task.order_status_javascript.trim() + `\n`)
+      }
+
+      if (description) {
+        readmeMd += `\n\n${description}`
+      }
+
+      if (Object.keys(task.options).length > 0) {
+        readmeMd += `\n\n## Default options`
+        readmeMd += `\n\n\`\`\`json\n${JSON.stringify(task.options, null, 2)}\n\`\`\``
+      }
+
+      if (`${task.subscriptions_template}`.trim()) {
+        readmeMd += `\n\n## Subscriptions`
+        readmeMd += `\n\n\`\`\`liquid\n${task.subscriptions_template}\n\`\`\``
+      }
+
+      if (`${task.docs}`.trim()) {
+        readmeMd += `\n\n## Documentation`
+        readmeMd += `\n\n${task.docs}`
+      }
+
+      readmeMd += `\n`
+
+      fs.writeFileSync(`${taskDocsDir}/README.md`, readmeMd)
+
+      if (!options.silent) {
+        process.stdout.write(`.`)
+      }
+
+    } catch (error) {
+      console.error(`Error during ${taskJsonPath}: ${error}`)
+    }
+  })
+
+  taskIndexMdLines.sort()
+
+  let taskIndexMd = `# Task documentation index`
+  taskIndexMd += `\n\nThis directory is built automatically. Each task's documentation is generated from the corresponding task export file in [../tasks](../tasks/). To contribute, see [../CONTRIBUTING.md](../CONTRIBUTING.md).`
+  taskIndexMd += `\n\n${taskIndexMdLines.join("\n")}\n`
+
+  fs.writeFileSync(`${docsDir}/README.md`, taskIndexMd)
+
+  if (!options.silent) {
+    process.stdout.write(`\n`)
+  }
+}
